bbafc2d4eb36659829e33027f8b44afd1ccb7931
Raphael Randschau <nicolai86@me.com>
2013-11-09 21:11:45 +0100

    static view works

--- app/models/static_view.rb
+++ app/models/static_view.rb
@@ -17,15 +17,7 @@ class StaticView < Flexirails::View
     Rails.application.routes.url_helpers.company_attendances_path(*args)
   end
 
-  def render_actions row, context
-    return context.render partial: 'actions', locals: { attendance: row }
-  end
-
-  def sortable_columns
-    return %w(id name)
-  end
-
   def columns
-    sortable_columns + %w(full_name)
+    %w(id name)
   end
 end
